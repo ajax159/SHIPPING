10e942669f20e13516c2621430c7cab03ac0798f
ajax159 <gmod159@gmail.com>
2024-06-08 16:49:57 -0500

    Update API endpoints

--- src/Modules/Op1/PaymentForm.jsx
+++ src/Modules/Op1/PaymentForm.jsx
@@ -117,7 +117,7 @@ export default function PaymentForm({Introsteps}) {
     } else{
       setTransportType(transportType);
       try {
-        fetch('http://190.223.58.252:5000/api/v1/service/aks-shippingia/score', {
+        fetch('/api/v1/service/aks-shippingia/score', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
