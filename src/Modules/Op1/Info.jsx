e85ff9cfd5a1e1eeb58a255a612ae60c7da6d11f
ajax159 <gmod159@gmail.com>
2024-06-11 08:34:21 -0500

    Update socket URL in Info.jsx

--- src/Modules/Op1/Info.jsx
+++ src/Modules/Op1/Info.jsx
@@ -47,7 +47,7 @@ const Info = ({ Introsteps }) => {
       return;
     } else {
       if (!isCameraRunning.current) {
-        socket = io('https://odshipping.onrender.com', {
+        socket = io('https://azurereverse.mercelab.com/', {
           mode: 'no-cors',
           transports: ['websocket'],
         });
